2e58ca8ff6870d5604a6063182af9fc2a9e0438d
vishvladi <vlad-vishnevski@mail.ru>
2023-06-18 01:00:03 +0300

    Кубы чисел от 1 до N

---
+++ hometask3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3 Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+ int ReadInt(string message)
+ {
+    System.Console.Write(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+int number = ReadInt("Введите целое положительное число: ");
+int count = 1;
+while (count <= number)
+{
+    Console.WriteLine(count * count * count);
+    count ++;
+}
